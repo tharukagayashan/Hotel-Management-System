ee8c0f79539206f1bb9092707ff02b74ee0c672c
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2022-04-24 01:31:59 +0530

    commit one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Navbar from './components/navbar.js';
+import { BrowserRouter as Router,Route, Routes} from 'react-router-dom';
+import Login from './components/login';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+      <Navbar/>
+        <Routes>
+          <Route path='/login' element={<Login/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
